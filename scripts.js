be5e2581bfe94ebc62de51a8a387c55c9f2e4936
Jefferson <102426788+JeffKeKa@users.noreply.github.com>
2022-10-21 16:05:12 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-function executar(){
+let nome = document.getElementById("inptUsuario")
+let senha = document.getElementById("inptSenha")
+let usuario = {
+    usernome:'',
+    passord:'',
+}
 
+function cadastrar(){
+
+    usuario.usernome = nome.value
+    usuario.passord = senha.value
+
+    alert(Object.values(usuario))
+    
 }
